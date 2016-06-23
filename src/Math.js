cec6ce87a4662f28acb3e9cf323312035869133e
John Hamm <johndhamm1@gmail.com>
2016-06-23 15:28:59 -0500

    all functions tested and passed

---
+++ src/Math.js
@@ -0,0 +1,31 @@
+function add(first, second) {
+	return first + second;
+}
+
+function subtract(first, second) {
+	return first - second;
+}
+
+function multiply(first, second){
+	return first * second;
+}
+
+function divide(first, second) {
+	return first / second;
+}
+
+function square (num) {
+	return num * num;
+}
+
+function squareRoot(num) {
+	return Math.sqrt(num);
+}
+
+
+
+
+
+
+
+
